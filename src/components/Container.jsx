a6f4431400b6635359af426e9297db7e17bf4b41
VihangaN <it18091380@my.sliit.lk>
2022-10-13 15:02:05 +0530

    add toggle switch and style update

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function Container({children}) {
+export default function Container({ children }) {
   return (
-   <div className="container-base">
-    {children}
-   </div>
-  )
+    <div className="container-base">
+      <div className="container-left"> {children[0]}</div>
+      <div className="container-right"> {children[1]}</div>
+    </div>
+  );
 }
