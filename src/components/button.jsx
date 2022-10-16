6a63c6a841e1e7d3cf4a2ca9413177a4480e1728
Pubudu Arosha <pubudu@derana.lk>
2022-10-17 04:25:39 +0530

    Add button component

---
+++ src/components/button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Button({
+  title,
+  component,
+  slug,
+  author,
+  gh_handle,
+}) {
+  return (
+    <div className="btn">
+       
+     
+     
+    </div>
+  );
+}
