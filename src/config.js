5a40073c834745c6f692336eeffbb0f69642ffa5
engineer-aslam-hossain <engineer.aslamhossain@gmail.com>
2022-10-21 15:16:11 +0600

    adding new count down component in the project.

--- src/config.js
+++ src/config.js
@@ -2,31 +2,37 @@ import CounterComponent from "./contribute/components/Counter";
 import TagComponent from "./contribute/components/Tag";
 import ToggleSwitch from "./contribute/components/ToggleSwitch";
 import TreeSelectInput from "./contribute/components/TreeSelect";
+import CountDown from "./contribute/components/CountDown";
 
 export const config = [
   {
     title: "Toggle switch",
-    component: <ToggleSwitch/>,
-    slug:"toggleswitch",
-    github_handle:"VihangaN"
+    component: <ToggleSwitch />,
+    slug: "toggleswitch",
+    github_handle: "VihangaN",
   },
   {
     title: "Tree Select",
-    component: <TreeSelectInput/>,
-    slug:"treeSelect",
-    github_handle:"VihangaN"
+    component: <TreeSelectInput />,
+    slug: "treeSelect",
+    github_handle: "VihangaN",
   },
   {
     title: "React Tag",
-    component: <TagComponent/>,
+    component: <TagComponent />,
     slug: "tag",
-    github_handle: "Dilshan97"
+    github_handle: "Dilshan97",
   },
   {
     title: "React Counter",
-    component: <CounterComponent/>,
+    component: <CounterComponent />,
     slug: "counter",
-    github_handle: "Dilshan97"
-  }
-  
+    github_handle: "Dilshan97",
+  },
+  {
+    title: "React CountDown",
+    component: <CountDown />,
+    slug: "countdown",
+    github_handle: "engineer-aslam-hossain",
+  },
 ];
