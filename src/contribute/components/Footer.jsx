9ca1e0c26d368b6134405ed82df70fd0a2c2b621
Pubudu Arosha <pubudu@derana.lk>
2022-10-30 23:26:31 +0530

    Add footer component.

---
+++ src/contribute/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <div style={{ display: 'inline-grid'}}>
+            <h5>- Footer -</h5>
+        </div>
+    )
+}
+
+export default Footer;
